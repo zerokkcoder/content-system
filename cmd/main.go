a38979b8287bc1bfffe5b2c7c0d64a9855120582
zerokx <918031078@qq.com>
2024-06-18 00:31:41 +0800

    use gin and add .gitignore

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	err := r.Run()
+	if err != nil {
+		fmt.Printf("r run error: %v", err)
+		return
+	}
 }
